01c0a2dab28b04817d77acf36bf0ed0b8b099ed4
Esther Halls <esthernhalls@gmail.com>
2023-02-27 13:09:30 -0700

    refactored storyboards

---
+++ iTunesSearch/Album Detail View/TrackTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TrackTableViewCell.swift
+//  iTunesSearch
+//
+//  Created by Esther on 2/27/23.
+//
+
+import UIKit
+
+class TrackTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
