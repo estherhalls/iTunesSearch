eb03639c322e3e21bd492e3e517d961277164b64
Esther Halls <esthernhalls@gmail.com>
2023-02-27 15:37:43 -0700

    created data task

--- iTunesSearch/Services/NetworkingController.swift
+++ iTunesSearch/Services/NetworkingController.swift
@@ -11,4 +11,28 @@ class NetworkingController {
     // Required Technologies: Codable, Result Type, Custom Errors, Capture Lists
     // Album Detail URL: https://itunes.apple.com/lookup?entity=song&id=320412048
     
+    private static let baseURLString = "https://itunes.apple.com/lookup"
+    
+    static func fetchAlbum(with url: URL, completion: @escaping(Result<Album, NetworkError>) -> Void) {
+        URLSession.shared.dataTask(with: url) { dTaskData, _, error in
+            if let error {
+                print("Encountered error: \(error.localizedDescription)")
+                completion(.failure(.thrownError(error)))
+            }
+            // Check for Data
+            guard let albumData = dTaskData else {
+                completion(.failure(.noData))
+                return
+            }
+            // Conveert to JSON (do, try, catch)
+            do {
+                let album = try JSONDecoder().decode(Album.self, from: albumData)
+                completion(.success(album))
+            } catch {
+                completion(.failure(.unableToDecode))
+                return
+            }
+            // Resume starts dataTask and continues it; tasks begin in suspended state
+        }.resume()
+    }
 } // End of Class
